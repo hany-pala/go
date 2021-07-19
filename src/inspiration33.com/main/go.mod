c36ae821d0f4daca65bc33560ec8dc539cba14ba
inspiration33 <inspiration33@kakao.com>
2021-07-19 23:29:24 +0900

    20. interface

--- src/inspiration33.com/main/go.mod
+++ src/inspiration33.com/main/go.mod
@@ -1,7 +1,14 @@
 module inspiration33.com/main
 
 go 1.16
-require inspiration33.com/src/utils v0.0.0-00010101000000-000000000000
+
+require (
+	inspiration33.com/src/interfaces v0.0.0-00010101000000-000000000000 // indirect
+	inspiration33.com/src/utils v0.0.0-00010101000000-000000000000
+)
 
 replace inspiration33.com/src => ../src
+
 replace inspiration33.com/src/utils => ../src/utils
+
+replace inspiration33.com/src/interfaces => ../src/interfaces
--- src/inspiration33.com/main/main.go
+++ src/inspiration33.com/main/main.go
@@ -1,24 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"inspiration33.com/src/interfaces"
+)
 
-type Stringer interface {
-	String() string
-}
-
-type Student struct {
-	Name string
-	Age  int
-}
-
-func (s Student) String() string {
-	return fmt.Sprintf("안녕! 나는 %d살 %s라고 해", s.Age, s.Name)
-}
 func main() {
-	student := Student{"철수", 12}
-	var stringer Stringer
-
-	stringer = student
-
-	fmt.Printf("%s\n", stringer.String())
+	interfaces.GetName()
 }
